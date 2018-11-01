c182267854530f20b4c6a74c99d8fe5fac4649f5
Harun R Rayhan <harun.b13@gmail.com>
2018-11-01 19:24:01 +0600

    Fist app build

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      location: "Seattle, WA",
+      location: "San Francisco, CA",
       animal: "",
       breed: "",
       breeds: [],
